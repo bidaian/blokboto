14beb81813fc24d9f93b62d98be85205300b6b2c
Eduardo Trápani <etrapani@gmail.com>
2018-10-25 15:05:42 -0300

    Adjusted path to media files

--- code.js
+++ code.js
@@ -383,7 +383,7 @@ Code.init = function() {
            length: 3,
            colour: '#ccc',
            snap: true},
-       media: '../../media/',
+       media: 'blockly-master/media/',
        rtl: rtl,
        toolbox: toolboxXml,
        zoom:
